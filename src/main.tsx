796353b7a8c56026267a2c436e0742d21b251692
Junsu Shin <junsushin4546@gmail.com>
2022-12-14 21:05:40 +0900

    feat: root, 생성, 수정, 조회, 확정 페이지 경로 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Root page</div>
+  },
+  {
+    path: 'meetings/new',
+    element: <div>Create a new meeting</div>
+  },
+  {
+    path: 'meetings/:meetingId',
+    element: <div>View a meeting</div>
+  },
+  {
+    path: 'meetings/:meetingId/edit',
+    element: <div>Edit a meeting</div>
+  },
+  {
+    path: 'meetings/:meetingId/result',
+    element: <div>View voting results for a meeting</div>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
