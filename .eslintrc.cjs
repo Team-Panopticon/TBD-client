217b78a10e45db5337d4946aee828acd0cad67d3
saeeng <saeeng.opt@gmail.com>
2022-12-15 10:51:08 +0900

    chore: lint ususedVar autofix off

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -54,8 +54,7 @@ module.exports = {
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/no-unsafe-return': 'warn',
     '@typescript-eslint/no-unsafe-assignment': 'warn',
-    '@typescript-eslint/no-unused-vars': 'error',
-    'autofix/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
   settings: {
     react: {
