70c2001cfcc95c5fd950404253e6c78cb5831dfd
saeeng <saeeng.opt@gmail.com>
2022-12-15 10:50:16 +0900

    Revert "feat: recoil 설치 및 sample code 작성"
    This reverts commit 644ecab49fc00c6c2ed5ddfb176b9bae91870142.

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { RecoilRoot } from 'recoil';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <App />
   </React.StrictMode>,
 );
