099207a001c4e85cdbe7b3abf7373e3704d769f3
Junsu Shin <junsushin4546@gmail.com>
2022-12-28 22:15:39 +0900

    feat: Input 컴포넌트와 Label 추가

--- src/pages/MeetingCreate.tsx
+++ src/pages/MeetingCreate.tsx
@@ -1,10 +1,21 @@
 import { Contents, Footer, Header, Page } from './styled';
+import { Input, Typography } from 'antd';
+import { InputContainer, InputLabel } from '../components/pages/MeetingCreate/styled';
 
 export function MeetingCreate() {
   return (
     <Page>
       <Header>Header Area</Header>
-      <Contents>Contents Area</Contents>
+      <Contents>
+        <InputContainer>
+          <div>
+            <InputLabel htmlFor="name">
+              <Typography>모임 이름</Typography>
+            </InputLabel>
+            <Input id="name" allowClear style={{ borderRadius: 0 }} />
+          </div>
+        </InputContainer>
+      </Contents>
       <Footer>Footer Area</Footer>
     </Page>
   );
