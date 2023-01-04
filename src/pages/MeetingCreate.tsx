805dd40751257bba16c9ed250195cfd3591f5524
Junsu Shin <junsushin4546@gmail.com>
2023-01-04 21:21:33 +0900

    feat: CreateMeetingState에 meetingName 상태 저장

--- src/pages/MeetingCreate.tsx
+++ src/pages/MeetingCreate.tsx
@@ -1,8 +1,19 @@
 import { Contents, Footer, Header, HeaderContainer, Page, PageTitle } from './styled';
 import { Input, Typography } from 'antd';
 import { InputContainer, InputLabel } from '../components/pages/MeetingCreate/styled';
+import { useRecoilState } from 'recoil';
+import { createMeetingState } from '../stores/createMeeting';
 
 export function MeetingCreate() {
+  const [meeting, setMeeting] = useRecoilState(createMeetingState);
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMeeting({
+      ...meeting,
+      name: event.target.value,
+    });
+  };
+
   return (
     <Page>
       <Header>
@@ -17,7 +28,7 @@ export function MeetingCreate() {
             <InputLabel htmlFor="name">
               <Typography>모임 이름</Typography>
             </InputLabel>
-            <Input id="name" allowClear style={{ borderRadius: 0 }} />
+            <Input id="name" allowClear style={{ borderRadius: 0 }} onChange={handleNameChange} />
           </div>
         </InputContainer>
       </Contents>
